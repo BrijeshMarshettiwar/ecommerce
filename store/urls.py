488aa01f0198a9e597bcaefc5af0f6129a1ca441
Brijesh Marshettiwar <123184136+BrijeshMarshettiwar@users.noreply.github.com>
2025-02-24 23:10:47 +0530

    Create urls.py

---
+++ store/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.store, name='store'),
+    path('cart/',views.cart,name='cart'),
+    path('checkout/',views.checkout,name='checkout'),
+    path('update_item/',views.updateItem,name='update_item'),
+    path('process_order/',views.processOrder,name='process_order'),
+    path('<int:id>',views.productDetail, name='productDetail'),
+    path('signup/',views.SignupPage, name='signup'),
+    path('login/',views.LoginPage, name='login'),
+    path('logout/',views.LogoutPage, name='logout'),
+    path('About',views.AboutusPage,name='About'),
+    path('Services',views.ServicesPage, name='Services'),
+    path('Contact',views.ContactPage, name='Contact')
+
+]
